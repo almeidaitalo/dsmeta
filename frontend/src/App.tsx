057a5e2933112c2cafa5a5552d61530ed784014b
unknown <italoalmeida733@gmail.com>
2022-11-14 19:17:24 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return(
+    <>
     <h1>Olá mundo!</h1>
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    </>
   )
 }
 
